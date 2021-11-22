2d4f9f5af0e6bfd1b3e74c9c5479ab20d4600afd
longwatermelon <longwaddermelon@gmail.com>
2021-11-21 17:39:16 -0700

    cut down on size by 32 bytes

--- sudont.s
+++ sudont.s
@@ -5,14 +5,13 @@ _start:
     mov rax, 1
     mov rdi, 1
     mov rsi, msg
-    mov rdx, len
+    mov rdx, 15
     syscall
 
     mov rax, 60
     mov rdi, 0
     syscall
 
-section .rodata
+section .data
     msg: db 'Nothing to do.', 10, 14
-    len: equ $ - msg
 
